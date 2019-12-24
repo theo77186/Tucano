93f8a643e35812b13b8c7cb5676f72181b775c06
theo77186 <theo77186@gmail.com>
2019-12-24 16:33:10 +0100

    Add 256GB hash support

--- src/globals.h
+++ src/globals.h
@@ -141,7 +141,7 @@ typedef unsigned int    UINT;
 #define MIN_THREADS     1
 #define MAX_THREADS     256
 #define MIN_HASH_SIZE   8
-#define MAX_HASH_SIZE   16384
+#define MAX_HASH_SIZE   262144
 
 // Piece index
 #define PAWN        0
